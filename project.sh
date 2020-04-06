99853a19e4c038d3e824d9a4cb0e8f0dcaa55a0e
André de Jager <andrethehunter@gmail.com>
2020-04-06 15:41:58 +1000

    Change script-invoke grep args

--- project.sh
+++ project.sh
@@ -134,7 +134,7 @@ script-invoke () {
 	elif [[ "$1" =~ ^(help|-h|--help)$ ]];then
 		usage
 		exit 0
-	elif (grep --quiet --extended-regexp --regexp="^$1 *?\\([\\t ]*?\\)" "$script_name");then
+	elif (grep -qE -e "^$1 *?\\([\\t ]*?\\)" "$script_name");then
 		"$@"
 	else
 		echo-error "Unknown function $1 ($script_name $*)"
